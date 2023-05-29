01ea76e1782e93ac9d9f2003c8f01a0a365eb67d
Brent Reynolds <brentsreynolds5@gmail.com>
2023-05-29 11:55:35 -0400

    Created deck of cards

---
+++ main.py
@@ -0,0 +1,8 @@
+cards = []
+suits = ["spades", "clubs", "hearts", "diamonds"]
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+for suit in suits:
+    for rank in ranks:
+     cards.append([suit, rank])
+
+print(cards)
